e5fea5e34cc3a5b15631219acb5e69ef38a6aa2d
Vadim <vadim2387@gmail.com>
2021-05-26 17:16:39 +0300

    lesson 5

---
+++ Meyer_Vadim_dz_5/task_5_1.py
@@ -0,0 +1,18 @@
+# Написать генератор нечётных чисел от 1 до n (включительно), используя ключевое слово yield, например:
+# >>> odd_to_15 = odd_nums(15)
+# >>> next(odd_to_15)
+# 1
+# >>> next(odd_to_15)
+# 3
+# ...
+# >>> next(odd_to_15)
+# 15
+# >>> next(odd_to_15)
+# ...StopIteration...
+
+def odd_nums():
+    for i in range(1, int(input('Введите число: ')) + 1, 2):
+        yield i
+print(*odd_nums())
+
+
